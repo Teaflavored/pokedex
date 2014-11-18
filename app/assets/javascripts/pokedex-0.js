b5b1dc4d12b23fdb3ce6c6547a53e8bf3d699e46
Auster Chen <auster.s.chen@gmail.com>
2014-11-17 18:49:22 -0800

    updates

--- app/assets/javascripts/pokedex-0.js
+++ app/assets/javascripts/pokedex-0.js
@@ -57,6 +57,7 @@ window.Pokedex.RootView = function ($el) {
   this.$el = $el;
   this.pokes = new Pokedex.Collections.Pokemon();
   this.$pokeList = this.$el.find('.pokemon-list');
+  // this.$pokeList = $("<div>");
   this.$pokeDetail = this.$el.find('.pokemon-detail');
   this.$newPoke = this.$el.find('.new-pokemon');
   this.$toyDetail = this.$el.find('.toy-detail');
